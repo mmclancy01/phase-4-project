cac7297022f9d00bd0ba76db4146cf18915682ed
Matthew Clancy <mmclancy01@yahoo.com>
2024-04-12 11:18:19 -0400

    backend update

--- server/models.py
+++ server/models.py
@@ -4,3 +4,67 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class WorkoutExercise(db.Model, SerializerMixin):
+    __tablename__ = 'workout_exercises'
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), primary_key=True)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), primary_key=True)
+    sets = db.Column(db.Integer, nullable=False)
+    reps = db.Column(db.Integer, nullable=False)
+    
+    workouts = db.relationship('Workout', back_populates = 'workout_exercises')
+    exercises = db.relationship('Exercise', back_populates = 'workout_exercises')
+    
+    serialize_rules = ['-workouts', '-exercises']
+
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    height = db.Column(db.Float, nullable=False)
+    goal_weight = db.Column(db.Integer)
+    workouts = db.relationship('Workout', back_populates = 'users')
+    # goals = db.relationship('Goal', back_populates = 'users')
+    
+    
+    serialize_rules = ['-workouts.users']
+
+class Workout(db.Model, SerializerMixin):
+    __tablename__ = 'workouts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text)
+    workout_exercises = db.relationship('WorkoutExercise', back_populates = 'workouts')
+    users = db.relationship('User', back_populates = 'workouts')
+    # user_workouts = db.relationship('UserWorkout', back_populates = 'workouts')
+
+    serialize_rules = ['workout_exercises', '-users', '-user_workouts']
+
+
+
+class Exercise(db.Model, SerializerMixin):
+    __tablename__ = 'exercises'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    muscle_group = db.Column(db.String)
+    push_pull = db.Column(db.String)
+    difficulty = db.Column(db.String)
+    
+    workout_exercises = db.relationship('WorkoutExercise', back_populates = 'exercises')
+
+    serialize_rules = ['muscle_group', 'name', 'push_pull', 'difficulty', '-workout_exercises']
+
+# class Goal(db.Model, SerializerMixin):
+#     __tablename__ = 'goals'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     start_weight = db.Column(db.Integer)
+#     goal_weight = db.Column(db.Integer)
+
+#     users = db.relationship('User', back_populates = 'goals')
+#     serialize_rules = ('user_id', '-users', 'users.username')
--- server/seed.py
+++ server/seed.py
@@ -5,13 +5,48 @@ from random import randint, choice as rc
 
 # Remote library imports
 from faker import Faker
+from datetime import date
 
 # Local imports
 from app import app
-from models import db
+from models import User, Exercise, Workout, WorkoutExercise
+from config import db
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        db.session.query(User).delete()
+        db.session.query(Workout).delete()
+        db.session.query(Exercise).delete()
+        
+        db.session.query(WorkoutExercise).delete()
+        db.session.commit()
         print("Starting seed...")
-        # Seed code goes here!
+        user1 = User(username='user1', age=25, weight=270.5, height=175.0, goal_weight = 215 )
+        user2 = User(username='user2', age=30, weight=265.2, height=168.0, goal_weight = 205)
+        
+        # Create workouts
+        workout1 = Workout(user_id=1, date=date(2024, 4, 10), duration=60, notes='Great workout!')
+        workout2 = Workout(user_id=2, date=date(2024, 4, 11), duration=45, notes='Feeling energized!')
+        workout3 = Workout(user_id=1, date=date(2024, 4, 12), duration=70, notes='Good workout!')
+        # Create exercises
+        exercise1 = Exercise(name='Squats', muscle_group='Legs', difficulty='Intermediate', push_pull = 'Legs')
+        exercise2 = Exercise(name='Bench Press', muscle_group='Chest', difficulty='Advanced', push_pull = 'Push')
+        
+        # Create goals
+        
+        # Add instances to the session
+        db.session.add_all([user1, user2, workout1, workout2, exercise1, exercise2, workout3])
+        
+        # Create workout-exercise associations
+        workout_exercise1 = WorkoutExercise(workout_id=1, exercise_id=1, sets=3, reps=10)
+        workout_exercise2 = WorkoutExercise(workout_id=2, exercise_id=2, sets=4, reps=8)
+        workout_exercise3 = WorkoutExercise(workout_id=3, exercise_id=1, sets=4, reps=8)
+        # Add workout-exercise associations to the session
+        db.session.add_all([workout_exercise1, workout_exercise2, workout_exercise3])
+        
+        # Commit the changes
+        db.session.commit()
+
+        
+
