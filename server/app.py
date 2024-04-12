362d2a474f441ab4d92edb10cfbf23e9e2414792
Delisma <delispence7@gmail.com>
2024-04-12 16:09:14 -0400

    added

--- server/app.py
+++ server/app.py
@@ -5,7 +5,8 @@
 # Remote library imports
 from flask import request
 from flask_restful import Resource
-
+from flask import Flask, request, jsonify
+from datetime import date, datetime
 
 # Local imports
 from config import app, db, api
@@ -43,11 +44,85 @@ def get_workouts():
 
 @app.post("/workouts")
 def post_workouts():
-    pass
+    data = request.get_json()
+
+    # Get the existing workouts count
+    workouts_count = Workout.query.count()
+
+    workout = Workout(
+        user_id=data['user_id'],
+        date=datetime.strptime(data['date'], '%Y-%m-%d').date(),
+        duration=data['duration'],
+        notes=data['notes']
+    )
+
+    db.session.add(workout)
+    db.session.commit()
+
+    response = {
+        "message": "Workout created successfully",
+        "workout": {
+            "id": workouts_count + 1,  # Calculate the new workout ID
+            "user_id": workout.user_id,
+            "date": workout.date.strftime('%Y-%m-%d'),
+            "duration": workout.duration,
+            "notes": workout.notes
+        }
+    }
+
+    return jsonify(response), 201
+
+@app.route("/workouts/<int:workout_id>", methods=["GET"])
+def get_workout(workout_id):
+    workout = Workout.query.get(workout_id)
+    if workout:
+        response = {
+            "workout_id": workout.id,
+            "user_id": workout.user_id,
+            "date": workout.date.strftime('%Y-%m-%d'),
+            "duration": workout.duration,
+            "notes": workout.notes
+        }
+        return jsonify(response), 200
+    else:
+        return jsonify({"message": "Workout not found"})
 
 @app.post("/exercises")
 def post_exercises():
-    pass
+    data = request.get_json()
+
+    exercise = Exercise(
+        name=data['name'],
+        muscle_group=data['muscle_group'],
+        difficulty=data['difficulty'],
+        push_pull=data['push_pull']
+    )
+
+    db.session.add(exercise)
+    db.session.commit()
+
+    response = {
+        "message": "Exercise created successfully",
+        "exercise": {
+            
+            "name": exercise.name,
+            "muscle_group": exercise.muscle_group,
+            "difficulty": exercise.difficulty,
+            "push_pull": exercise.push_pull
+        }
+    }
+
+    return jsonify(response), 201
+@app.route("/exercises/<int:exercise_id>", methods=["DELETE"])
+def delete_exercise(exercise_id):
+    exercise = Exercise.query.get(exercise_id)
+
+    if exercise:
+        db.session.delete(exercise)
+        db.session.commit()
+        return jsonify({"message": "Exercise deleted successfully"}), 200
+    else:
+        return jsonify({"message": "Exercise not found"}), 404
 
 
 if __name__ == '__main__':
