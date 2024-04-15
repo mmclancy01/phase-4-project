56f710828eca0c271114a287fb4bb544b19014b4
cardenas1995 <154838641+cardenas1995@users.noreply.github.com>
2024-04-15 10:52:27 -0400

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar';
-import HeroSection from './HeroSection';
-import FeatureSection from './FeatureSection';
-import Footer from './Footer';
-import Profile from './Profile'; // Import your Profile component
-import Exercises from './Exercises'; // Import your Exercises component
-import Activity from './Activity'; // Import your Activity component
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import FeatureSection from './components/FeatureSection';
+import Footer from './components/Footer';
+import Profile from './components/Profile'; // Import your Profile component
+import Exercises from './components/Exercises'; // Import your Exercises component
+import Activity from './components/Activity'; // Import your Activity component
 import './App.css'; // Import CSS file
 
 function App() {
