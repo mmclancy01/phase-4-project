6213a1eacc2aec24912a24930b3dbec0367ab987
cardenas1995 <154838641+cardenas1995@users.noreply.github.com>
2024-04-17 16:02:03 -0400

    update css

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,11 +1,17 @@
-// Profile.js
 import React from 'react';
 
-const Profile = () => {
+
+const Profile = ({ name, height, weight, weightGoal }) => {
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <p>User Profile Information</p>
+    <div className="container">
+      <div className="profile-info">
+        <h1>{name}🏋️‍♂️ </h1>
+        <h2>Profile </h2>
+        <p><strong>Height:</strong> {height} cm</p>
+        <p><strong>Weight:</strong> {weight} kg</p>
+        <p><strong>Weight Goal:</strong> {weightGoal} kg</p>
+      </div>
+      <img className="profile-image" src="https://cdn4.iconfinder.com/data/icons/medical-icons-rounded-vector/1250/path_health_calculator-512.png" alt="Health Calculator" />
     </div>
   );
 };
